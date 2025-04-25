c4f15450697529c4121f0846401d2e67e2090a18
Shaggy42089 <thiblego@gmail.com>
2025-04-25 23:29:55 +0200

    added all the stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Shaggy</title>
+</head>
+<body>
+	Welcome to my website !
+</body>
+</html>
+
+{@render children()}
