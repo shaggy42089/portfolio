953536126a2ba2b1b9721455decf4975ed1a0615
Shaggy42089 <thiblego@gmail.com>
2025-04-28 20:15:52 +0200

    added navbar and daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,32 @@
 	let { children } = $props();
 </script>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Shaggy</title>
-</head>
-<body>
-	Welcome to my website !
-</body>
-</html>
 
+<div class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        </div>
+        <ul
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+          <li><a href="">Skills</a></li>
+          <li><a href="">Education</a></li>
+          <li><a href="">Career</a></li>
+        </ul>
+      </div>
+      <a class="btn btn-ghost text-xl navbar-center lg:inline hidden">Shaggy</a>
+    </div>
+    <a class="btn btn-ghost text-xl lg:hidden">Shaggy</a>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="">Skills</a></li>
+        <li><a href="">Education</a></li>
+        <li><a href="">Career</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <a class="btn btn-primary">Hire Me</a>
+    </div>
+  </div>
 {@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+
+<p>Welcome to shaggy's</p>
+<button class="btn btn-primary">Subscribe</button>
